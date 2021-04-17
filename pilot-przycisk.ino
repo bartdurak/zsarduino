bbf5a46ba7481dd36dc584eb7e6453e62bd073b0
mechatronikaedu <63419495+projektar22@users.noreply.github.com>
2021-04-17 17:33:23 +0200

    Add files via upload

---
+++ pilot-przycisk.ino
@@ -0,0 +1,30 @@
+#include <IRremote.h>
+#define irPin 11
+IRrecv irrecv(irPin);
+decode_results results;
+
+#define diodaPin 5
+int diodaStatus = LOW;
+
+void setup() {
+   Serial.begin(9600);
+    irrecv.enableIRIn();
+
+    pinMode(diodaPin, OUTPUT);
+}
+
+void loop() {
+   if (irrecv.decode(&results)) {
+
+      switch (results.value) {
+         case 0x10C8D12E:
+            Serial.println("srodek");
+            diodaStatus = ~diodaStatus;
+            digitalWrite(diodaPin, diodaStatus);
+            delay(250);
+            break;
+         }
+
+   irrecv.resume();
+   }
+}
